eb7e18fb211f1543136401b267f9c9615c47a5d1
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-10-04 19:37:39 +0200

    style progress bar

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -3,8 +3,7 @@ import React from "react";
 const ProgressBar = ({ width }) => {
   return (
     <progress id="file" value={width} max="100" style={{ width: "100%" }}>
-      {" "}
-      32%{" "}
+      {width}%
     </progress>
   );
 };
