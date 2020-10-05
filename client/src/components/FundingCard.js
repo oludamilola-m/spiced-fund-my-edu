d364180432f9f287db5e6803dba7dfc3a8537b1e
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-10-05 18:10:06 +0200

    move fundung card to component

---
+++ client/src/components/FundingCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ProgressBar from "../components/ProgressBar";
+import { Link } from "react-router-dom";
+
+const FundingCard = ({ funding }) => {
+  return (
+    <div className="funding-card" key={funding.id.toString()}>
+      <div>
+        <Link to={`/fundings/${funding.id}`}>
+          <img src={funding.image_url} alt="funding image" />
+        </Link>
+      </div>
+      <div className="funding-card__content">
+        <Link to={`/fundings/${funding.id}`}>
+          <h2>{funding.title}</h2>
+        </Link>
+        <span className="muted">
+          {" "}
+          <strong>${funding.donated_amount}</strong>
+        </span>
+        <ProgressBar width={funding.progress} />
+        <div className="funding-card__progress-goal">
+          <span className="funding-card__progress-goal__left">
+            <strong>{funding.progress}% Donated</strong>
+          </span>
+          <span className="funding-card__progress-goal__right muted">
+            <strong>Goal: ${funding.total_amount}</strong>
+          </span>
+        </div>
+
+        <p className="funding-card__description">{funding.short_description}</p>
+
+        <Link to={`/fundings/${funding.id}/donate`}>
+          <button className="funding-card__btn">Donate Now</button>{" "}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FundingCard;
--- client/src/pages/Fundings.js
+++ client/src/pages/Fundings.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
-import ProgressBar from "../components/ProgressBar";
+
+import FundingCard from "../components/FundingCard";
 
 const Fundings = () => {
   const [fundings, setFundings] = useState([]);
@@ -18,41 +18,7 @@ const Fundings = () => {
   return (
     <main className="fundings-container">
       {fundings.map((funding) => {
-        return (
-          <div className="funding-card" key={funding.id.toString()}>
-            <div>
-              <Link to={`/fundings/${funding.id}`}>
-                <img src={funding.image_url} alt="funding image" />
-              </Link>
-            </div>
-            <div className="funding-card__content">
-              <Link to={`/fundings/${funding.id}`}>
-                <h2>{funding.title}</h2>
-              </Link>
-              <span className="muted">
-                {" "}
-                <strong>${funding.donated_amount}</strong>
-              </span>
-              <ProgressBar width={funding.progress} />
-              <div className="funding-card__progress-goal">
-                <span className="funding-card__progress-goal__left">
-                  <strong>{funding.progress}% Donated</strong>
-                </span>
-                <span className="funding-card__progress-goal__right muted">
-                  <strong>Goal: ${funding.total_amount}</strong>
-                </span>
-              </div>
-
-              <p className="funding-card__description">
-                {funding.short_description}
-              </p>
-
-              <Link to={`/fundings/${funding.id}/donate`}>
-                <button className="funding-card__btn">Donate Now</button>{" "}
-              </Link>
-            </div>
-          </div>
-        );
+        return <FundingCard key={funding.id.toString()} funding={funding} />;
       })}
     </main>
   );
